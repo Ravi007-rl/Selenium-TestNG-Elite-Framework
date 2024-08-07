c5b6fac03336bb83c4f13f7d993c806b7c6e2fa2
Ravi007-rl <ravi007.rl@gmail.com>
2024-08-07 11:57:58 +0530

    Commit: 001 - Started working on wait helper class.

---
+++ src/test/java/com/selenium/testng/elite/webTest/LoginTest.java
@@ -0,0 +1,12 @@
+package com.selenium.testng.elite.webTest;
+
+import com.selenium.testng.elite.BaseTest;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void VerifyThatUserIsAbleToLogin(){
+
+    }
+}
