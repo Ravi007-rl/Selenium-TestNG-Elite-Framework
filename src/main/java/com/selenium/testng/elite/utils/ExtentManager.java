3a91ca7ccd4ecc1897869f60c543a657f27df4e4
Ravi007-rl <ravi007.rl@gmail.com>
2024-08-20 11:25:17 +0530

    Commit: 009 - Fixed view order and log issue in report

--- src/main/java/com/selenium/testng/elite/utils/ExtentManager.java
+++ src/main/java/com/selenium/testng/elite/utils/ExtentManager.java
@@ -2,6 +2,7 @@ package com.selenium.testng.elite.utils;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.ViewName;
 
 public class ExtentManager {
   private static ExtentReports extent;
@@ -9,7 +10,15 @@ public class ExtentManager {
   public static synchronized ExtentReports getInstance() {
     if (extent == null) {
       extent = new ExtentReports();
-      ExtentSparkReporter sparkReporter = new ExtentSparkReporter(PathHelper.getPathForReport());
+      ExtentSparkReporter sparkReporter =
+          new ExtentSparkReporter(PathHelper.getPathForReport())
+              .viewConfigurer()
+              .viewOrder()
+              .as(
+                  new ViewName[] {
+                    ViewName.DASHBOARD, ViewName.TEST, ViewName.EXCEPTION,
+                  })
+              .apply();
       extent.attachReporter(sparkReporter);
     }
     return extent;
